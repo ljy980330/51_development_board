ee896fd78033937cef0fb9e15b22e1dc0a1c5f11
leeszeki <158444396@qq.com>
2019-07-05 21:21:24 +0800

    changes
    changes

---
+++ software/底层驱动/001 I2C驱动/i2c_set/i2c_set.h
@@ -0,0 +1,35 @@
+/*********************************************************************************************************
+*名称：i2c_set.h
+*功能：	i2c设置所有用到的头文件以及函数的声明
+创建时间：2019/7/5
+修改时间：2019/XX/XX
+作者：XXX
+**********************************************************************************************************/
+
+#ifndef _I2C_SET_H
+#define _I2C_SET_H
+
+#include "config.h"
+#include "intrins.h"
+#include "STC12C5A.H"
+
+#define AT24C02_ADDR  0xa0	//AT24C02地址
+#define high 1	//高电平
+#define low  0	//低电位
+
+sbit SCL = P2^0;	//串行时钟
+sbit SDA = P2^1;	//串行数据
+
+void I2C_Init();
+
+void I2C_Start();
+
+void I2C_Stop();
+
+void Master_ACK(bit i);
+
+bit Test_ACK();
+
+
+#endif
+
