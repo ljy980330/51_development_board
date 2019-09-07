e89e727918f8587108650406c685a984404a9734
greensJadeSoup <1252091009@qq.com>
2019-09-07 19:45:31 +0800

    9.7更新(3)

--- software/初级例程/012 温度传感器/includes.h
+++ software/初级例程/012 温度传感器/includes.h
@@ -9,7 +9,8 @@
 #ifndef _INCLUDES_H
 #define _INCLUDES_H
 
-#include "STC12C5A.H"
+#include "STC12C5A.H"				 
+#include "stdio.h"
 #include "config.h"
 #include "OLED_Init/OLED_Init.h"
 #include "delay/delay.h"
@@ -19,5 +20,17 @@
 #include "i2c/i2c.h"
 #include "drive/drive.h"
 
+extern void PrintUartInit(void);
+void PrintUartInit(void)//使用定时器1作为串口波特率发生器
+{
+    TH1 = 0xFD;	 //晶振11.0592mhz 波特率设为9600
+    TL1 = TH1;
+    TMOD |= 0x20;	 //定时器1方式2
+    SCON = 0x50;	 //串口接收使能
+    ES = 1;			 //串口中断使能
+    TR1 = 1;		 //定时器1使能
+    TI = 1;			 //发送中断标记位，必须设置
+	EA = 1;
+}
 
 #endif
