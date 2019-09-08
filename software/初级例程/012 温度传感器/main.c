47207da51e6d6992a9eff1ce5adbddd810b6cb18
ljy980330 <liangjingyan0330@hotmail.com>
2019-09-08 15:20:50 +0800

    update 190908

--- software/初级例程/012 温度传感器/main.c
+++ software/初级例程/012 温度传感器/main.c
@@ -18,24 +18,24 @@ int main()
 {
 	P3M0=0X00; //0000,0000 端口3模式寄存器0
 	P3M1=0X00;
-	//sys_init();
-#ifdef PRINT_LOG
-	PrintUartInit();
-#endif
+	sys_init();
+	
 	while(1)
 	{
-#ifdef PRINT_LOG
-		printf("-------DHT_Collect start.....-----------\n\r");
-#endif
 		DHT_Collect();//DHT11数据采集
-#ifdef PRINT_LOG
-		printf("-------DHT_Collect end.....-----------\n\r");
-#endif
 		//YLED=1;RLED=0;//灯状态转换
-		//OLED_Display();//OLED显示函数
+		OLED_Display();//OLED显示函数
 	//	RLED=1;YLED=0;//灯状态转换
 		delay_ms(1000);
 		delay_ms(1000);//注意：长时间的延时必须要有，不然会读出0
+		delay_ms(1000);
+		delay_ms(1000);
+		delay_ms(1000);
+		delay_ms(1000);
+		delay_ms(1000);
+		delay_ms(1000);
+//		Delay_1s();
+//		Delay_1s();
 	}
 }
 
